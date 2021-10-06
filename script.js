e51191bb4e00ff361ea963a3f5597eb937732bd5
Guilherme <mttsguil@gmail.com>
2021-10-06 16:13:15 -0300

    QueDiaEuNasciProjeto1

--- script.js
+++ script.js
@@ -1,25 +1,10 @@
-function media() { 
-    let nota1 = document.getElementById('nota1')
-    let nota2 = document.getElementById('nota2')
-    let nota3 = document.getElementById('nota3')
-    let nota4 = document.getElementById('nota4')
-    let res = document.getElementById('res')
+function media() {
+    let data = document.getElementById("date")
+var d = new Date(date.value)
+var dias = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"]
+console.log(dias[d.getDay()]) 
 
-    
+let res = document.getElementById("res")
+res.innerHTML = `Você nasceu no dia da semana: ${dias[d.getDay()]}.`
 
-
-    function calc(n1,n2,n3,n4) { 
-        return (n1+n2+n3+n4) /4 
-    }
-
-    let n1 = Number(nota1.value)
-    let n2 = Number(nota2.value)
-    let n3 = Number(nota3.value)
-    let n4 = Number(nota4.value)
-
-    var m = Number(calc(n1,n2,n3,n4))
-
-    res.innerHTML = `Sua média é ${m}`
-
-    
 }
